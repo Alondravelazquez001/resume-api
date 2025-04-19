1c85baf9bc9681bae91a5510dc7e8eff1a3e0d2f
alondra <alondra.velazquez001@mymdc.net>
2025-04-18 23:10:46 -0400

    Add simple CI/CD test

--- tests/integration/test_api_gateway.py
+++ tests/integration/test_api_gateway.py
@@ -43,3 +43,7 @@ class TestApiGateway:
 
         assert response.status_code == 200
         assert response.json() == {"message": "hello world"}
+    def test_ci_cd_is_working():
+    assert True
+
+
