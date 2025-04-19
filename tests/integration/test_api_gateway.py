7711697ca9760b0b14202fa544543a64e5933e81
alondra <alondra.velazquez001@mymdc.net>
2025-04-18 23:30:00 -0400

    Fix test file: move test_ci_cd_is_working outside of class

--- tests/integration/test_api_gateway.py
+++ tests/integration/test_api_gateway.py
@@ -1,19 +1,17 @@
 import os
-
 import boto3
 import pytest
 import requests
 
 """
-Make sure env variable AWS_SAM_STACK_NAME exists with the name of the stack we are going to test. 
+Make sure env variable AWS_SAM_STACK_NAME exists with the name of the stack we are going to test.
 """
-
 
 class TestApiGateway:
 
     @pytest.fixture()
     def api_gateway_url(self):
-        """ Get the API Gateway URL from Cloudformation Stack outputs """
+        """ Get the API Gateway URL from CloudFormation Stack outputs """
         stack_name = os.environ.get("AWS_SAM_STACK_NAME")
 
         if stack_name is None:
@@ -25,7 +23,8 @@ class TestApiGateway:
             response = client.describe_stacks(StackName=stack_name)
         except Exception as e:
             raise Exception(
-                f"Cannot find stack {stack_name} \n" f'Please make sure a stack with the name "{stack_name}" exists'
+                f"Cannot find stack {stack_name} \n"
+                f'Please make sure a stack with the name "{stack_name}" exists'
             ) from e
 
         stacks = response["Stacks"]
@@ -35,7 +34,7 @@ class TestApiGateway:
         if not api_outputs:
             raise KeyError(f"HelloWorldAPI not found in stack {stack_name}")
 
-        return api_outputs[0]["OutputValue"]  # Extract url from stack outputs
+        return api_outputs[0]["OutputValue"]  # Extract URL from stack outputs
 
     def test_api_gateway(self, api_gateway_url):
         """ Call the API Gateway endpoint and check the response """
@@ -43,8 +42,10 @@ class TestApiGateway:
 
         assert response.status_code == 200
         assert response.json() == {"message": "hello world"}
-    def test_ci_cd_is_working():
-        assert True
 
+
+# ✅ Simple test to make sure CI/CD is running
+def test_ci_cd_is_working():
+    assert True
 
 
